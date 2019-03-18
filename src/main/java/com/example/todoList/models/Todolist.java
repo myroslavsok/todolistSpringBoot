307dcc63eef3221d1341373bd03f0d0e6bb5429d
InterLink Intern <intern@intern-pc.interlink.office>
2019-03-18 18:17:44 +0200

    Adding dependencies postgre

--- src/main/java/com/example/todoList/models/Todolist.java
+++ src/main/java/com/example/todoList/models/Todolist.java
@@ -1,19 +1,32 @@
 package com.example.todoList.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Todolist {
-    public long id;
-    public String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
 
     public Todolist(String name) {
         this.name = name;
     }
 
-//    public long getId() {
-//        return this.id;
-//    }
+    public void setName(String text) {
+        this.name = text;
+    }
 
-//    public String getName() {
-//        return name;
-//    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
